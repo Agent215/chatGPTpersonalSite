e2716d915a440fc7712d69796d72124057a1df2f
Brahm <abraham.schultz@gmail.com>
2022-12-21 18:29:24 -0500

    adding built images and captions

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -3,34 +3,36 @@ import { Card } from 'react-bootstrap';
 import Accordion from 'react-bootstrap/Accordion';
 
 const Blog = (props) => {
-  const { blogs } = props;
+    const { blogs } = props;
 
-  return (
-    // always have open the latest blog
-    <Accordion defaultActiveKey={[blogs.length-1]} alwaysOpen style={{ paddingTop: '10vh', paddingBottom: '10vh' }}>
-      {blogs.map((blog, index) => (
-        <Accordion.Item eventKey={index}>
-          <Accordion.Header>{blog.date}</Accordion.Header>
-          <Accordion.Body>
-            <Card>
-              <Card.Header>{blog.emoji}</Card.Header>
-              <Card.Body>
-                <h5 className="title">{blog.title}</h5>
-                {blog.image && (
-                  <Card.Img src={blog.image} />
-                )}
-                <div className="content">
-                  {blog.content.split('\n').map((paragraph, index) => (
-                    <p key={index}>{paragraph}</p>
-                  ))}
-                </div>
-              </Card.Body>
-            </Card>
-          </Accordion.Body>
-        </Accordion.Item>
-      ))}
-    </Accordion>
-  );
+    return (
+        // always have open the latest blog
+        <Accordion defaultActiveKey={[blogs.length - 1]} alwaysOpen style={{ paddingTop: '10vh', paddingBottom: '10vh' }}>
+            {blogs.map((blog, index) => (
+                <Accordion.Item eventKey={index}>
+                    <Accordion.Header>{blog.date}</Accordion.Header>
+                    <Accordion.Body>
+                        <Card>
+                            <Card.Header>{blog.emoji}</Card.Header>
+                            <Card.Body>
+                                <h5 className="title">{blog.title}</h5>
+                                {blog.image && (
+                                    <Card.Img src={blog.image} />
+                                )} {blog.imageCaption && (
+                                    <Card.Text className="image-caption">{blog.imageCaption}</Card.Text>
+                                )}
+                                <div className="content">
+                                    {blog.content.split('\n').map((paragraph, index) => (
+                                        <p key={index}>{paragraph}</p>
+                                    ))}
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </Accordion.Body>
+                </Accordion.Item>
+            ))}
+        </Accordion>
+    );
 };
 
 export default Blog;

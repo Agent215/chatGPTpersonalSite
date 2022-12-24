c60a435e1dfad8c5cee920225bafac4ab41761f4
Brahm <abraham.schultz@gmail.com>
2022-12-24 02:08:43 -0500

    testing new blog stuff

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,44 +1,38 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
-import Accordion from 'react-bootstrap/Accordion';
-import { DiscussionEmbed } from 'disqus-react';
-import Disqus from "disqus-react"
+import { Card, Col, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Blog = (props) => {
-    const { blogs } = props;
+  const { blogs } = props;
 
-    return (
-        // always have open the latest blog
-        <Accordion defaultActiveKey={[blogs.length - 1]} alwaysOpen style={{ paddingTop: '10vh', paddingBottom: '10vh' }}>
-            {blogs.map((blog, index) => (
-                <Accordion.Item eventKey={index}>
-                    <Accordion.Header>{blog.date}</Accordion.Header>
-                    <Accordion.Body>
-                        <Card>
-                            <Card.Header>{blog.emoji}</Card.Header>
-                            <Card.Body>
-                                <h5 className="title">{blog.title}</h5>
-                                {blog.image && (
-                                    <Card.Img src={blog.image} />
-                                )} {blog.imageCaption && (
-                                    <Card.Text className="image-caption">{blog.imageCaption}</Card.Text>
-                                )}
-                                <div className="content">
-                                    {blog.content.split('\n').map((paragraph, index) => (
-                                        <p key={index}>{paragraph}</p>
-                                    ))}
-                                </div>
-                            </Card.Body>
-                            <Card.Body>
-                                <script defer src="https://cdn.commento.io/js/commento.js"></script>
-                                <div id="commento"></div>
-                            </Card.Body>
-                        </Card>
-                    </Accordion.Body>
-                </Accordion.Item>
-            ))}
-        </Accordion>
-    );
+  return (
+    <Row>
+      {blogs.map((blog, index) => (
+        <Col xs={12} md={4} key={index}>
+          <Link style={{ textDecoration: 'none', color: 'black' }}  to={`/blog/${blog.id}`}>
+            <Card style={{ margin: '1rem' }}>
+              <Card.Body>
+                <h5 className="title" style={{ textDecoration: 'none', color: 'black' }}>
+                  {blog.title}
+                </h5>
+                {blog.image && (
+                  <Card.Img src={blog.image} />
+                )}
+                <Card.Text className="date" style={{ textDecoration: 'none', paddingTop: '1rem' }}>
+                  {blog.date}
+                </Card.Text>
+                {blog.imageCaption && (
+                  <Card.Text  style={{ textDecoration: 'none', paddingTop: '1rem' }}>
+                    {blog.imageCaption}
+                  </Card.Text>
+                )}
+              </Card.Body>
+            </Card>
+          </Link>
+        </Col>
+      ))}
+    </Row>
+  );
 };
 
 export default Blog;
---
+++ src/Components/BlogPage.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import { DiscussionEmbed } from 'disqus-react';
+import {getBlogById} from './../getBlogById'
+import { useParams } from "react-router-dom";
+
+const BlogPage = () => {
+    const { id } = useParams();
+    const blog = getBlogById(id);
+
+  return (
+    <Card style={{margin:'1rem'}}>
+      <Card.Header>{blog.emoji}</Card.Header>
+      <Card.Body>
+        <h5 className="title">{blog.title}</h5>
+        {blog.image && (
+          <Card.Img src={blog.image} />
+        )}
+        {blog.imageCaption && (
+          <Card.Text className="image-caption">{blog.imageCaption}</Card.Text>
+        )}
+        <div className="content">
+          {blog.content.split('\n').map((paragraph, index) => (
+            <p key={index}>{paragraph}</p>
+          ))}
+        </div>
+      </Card.Body>
+      <Card.Body>
+        <DiscussionEmbed
+        shortname='brahm'
+          config={
+            {
+              url: window.location.href,
+              identifier: blog.id,
+              title: blog.title,
+            }
+          }
+        />
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default BlogPage;

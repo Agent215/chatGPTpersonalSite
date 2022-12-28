62f50fafc07c64c4563e2556f353b25699ab6ebe
Brahm <abraham.schultz@gmail.com>
2022-12-27 20:51:36 -0500

    fixing typos, editing stlye, disqus shortname brahmschultz

--- src/Components/BlogPage.js
+++ src/Components/BlogPage.js
@@ -1,54 +1,57 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import { DiscussionEmbed } from 'disqus-react';
-import { getBlogById } from './../getBlogById'
+import { getBlogById } from './../getBlogById';
 import { useParams } from "react-router-dom";
 import { CodeBlock } from './CodeBlock';
 import { ImageCaption } from './ImageCaption';
 
-const BlogPage = () => {
-    const { id } = useParams();
-    const blog = getBlogById(id);
 
-    return (
-        <Card className="blog-card" style={{ margin: '1rem' }}>
-            <Card.Header>{blog.emoji}</Card.Header>
-            <Card.Body>
-                <h5 className="title">{blog.title}</h5>
-                {blog.image && (
-                    <Card.Img src={blog.image} />
-                )}
-                {blog.imageCaption && (
-                    <Card.Text className="image-caption">{blog.imageCaption}</Card.Text>
-                )}
-                <div className="content">
-                    {blog.content.map((element, index) => {
-                        if (typeof element === 'string' && element.includes('<a')) {
-                            return (
-                                <div key={index} dangerouslySetInnerHTML={{ __html: element }} />
-                            );
-                        } else if (typeof element === 'string') {
-                            return <p key={index}>{element}</p>;
-                        } else if (element.code) {
-                            return <CodeBlock key={index} code={element.code} />;
-                        } else {
-                            return <ImageCaption key={index} image={element.image} imageCaption={element.imageCaption} />;
-                        }
-                    })}
-                </div>
-            </Card.Body>
-            <Card.Body>
-                <DiscussionEmbed
-                    shortname="brahm"
-                    config={{
-                        url: window.location.href,
-                        identifier: blog.id,
-                        title: blog.title,
-                    }}
-                />
-            </Card.Body>
-        </Card>
-    );
+
+
+const BlogPage = () => {
+  const { id } = useParams();
+  const blog = getBlogById(id);
+
+  return (
+    <Card className="blog-card" style={{ margin: '1rem' }}>
+      <Card.Header>{blog.emoji}</Card.Header>
+      <Card.Body >
+        <h5 className="title">{blog.title}</h5>
+        {blog.image && (
+          <Card.Img src={blog.image} className="card-img blog-page-image" />
+        )}
+        {blog.imageCaption && (
+          <Card.Text className="image-caption">{blog.imageCaption}</Card.Text>
+        )}
+        <div className="content">
+          {blog.content.map((element, index) => {
+            if (typeof element === 'string' && element.includes('<a')) {
+              return (
+                <div key={index} dangerouslySetInnerHTML={{ __html: element }} />
+              );
+            } else if (typeof element === 'string') {
+              return <p key={index}>{element}</p>;
+            } else if (element.code) {
+              return <CodeBlock key={index} code={element.code} />;
+            } else {
+              return <ImageCaption key={index} image={element.image} imageCaption={element.imageCaption} />;
+            }
+          })}
+        </div>
+      </Card.Body>
+      <Card.Body>
+        <DiscussionEmbed
+          shortname="brahmschultz"
+          config={{
+            url: window.location.href,
+            identifier: blog.id,
+            title: blog.title,
+          }}
+        />
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default BlogPage;
